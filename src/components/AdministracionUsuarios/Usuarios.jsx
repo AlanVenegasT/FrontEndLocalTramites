8e68e1255330c93914665b0ccac37b3e853bf4e3
Daniel <salasdaniel686@gmail.com>
2023-11-24 22:53:44 -0600

    Paginate Usuarios

--- src/components/AdministracionUsuarios/Usuarios.jsx
+++ src/components/AdministracionUsuarios/Usuarios.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import Alerta from "../Alerta";
 import useUsuario from "../../hooks/useUsuario";
 import ListUsuarios from "./ListUsuarios";
 import ModalCrearUsuario from "./ModalCrearUsuario";
+import Pagination from "../Tramites/Pagination";
 // import ModalCrearUsuario from
 
 const Usuarios = () => {
@@ -14,6 +14,8 @@ const Usuarios = () => {
   const [usuarios, setUsuarios] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   //form 
+  const [totalUsuarios, setTotalUsuarios] = useState(0);
+  const [paginate, setPaginate] = useState(1);
   const [nombre, setNombre] = useState("");
   const [correo, setCorreo] = useState("");
   const [estado, setEstado] = useState("");
@@ -25,7 +27,12 @@ const Usuarios = () => {
   useEffect(() => {
     setReload(false);
     const mostrarUsuarios = async () => {
-      const { data } = await consultarUsuarios();
+      const { data } = await consultarUsuarios(10, paginate);
+      if (data.data.data.length === 0) {
+        setPaginate(1);
+        setReload(true);
+      }
+      setTotalUsuarios(data.data.total);
       const response = data.data.data;
       setUsuarios(response);
     };
@@ -76,6 +83,20 @@ const Usuarios = () => {
     setReload(true);
   };
 
+  const siguientePage = () => {
+    setPaginate(paginate + 1);
+    setReload(true);
+  };
+
+  const anteriorPage = () => {
+    if (paginate === 0) {
+      setPaginate(1);
+    } else {
+      setPaginate(paginate - 1);
+    }
+    setReload(true);
+  };
+
   //La función hanldeSelectProyecto se utiliza cuando se hace clic en el botón de editar en ListProyectos. Actualiza el estado proyectoSelected con la información del proyecto seleccionado y abre el modal.
   const handleSelectUsuario = (usuario) =>{
     setUsuarioSelected(usuario);
@@ -88,7 +109,7 @@ const Usuarios = () => {
     <div className="px-4 sm:px-6 lg:px-8 lg:mt-20 xl:mt-20">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">Usuarios</h1>
+          <h1 className="text-base font-semibold leading-6 text-gray-900">Usuarios: {totalUsuarios}</h1>
           
         </div>
         
@@ -142,6 +163,11 @@ const Usuarios = () => {
             </table>
           </div>
         </div>
+        <Pagination
+            paginate={paginate}
+            anteriorPage={anteriorPage}
+            siguientePage={siguientePage}
+          />
       </div>
     </div>
   )
